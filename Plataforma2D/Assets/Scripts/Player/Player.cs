6e8dd992814c2630597cc9b68eb1d73778d834dd
Pedro Henrique J de Lima <pedrohjl03@gmail.com>
2024-02-03 09:21:53 -0300

    Jump Prototype
    Jump and friction

--- Plataforma2D/Assets/Scripts/Player/Player.cs
+++ Plataforma2D/Assets/Scripts/Player/Player.cs
@@ -8,20 +8,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     public Rigidbody2D myRigidBody;
 
-    public UnityEngine.Vector2 velocity;
+    public UnityEngine.Vector2 friction = new UnityEngine.Vector2(.1f, 0);
+
+    public float jumpForce = 2;
 
     public float speed;
     private void Update()
     {
+        handleJump();
+        handleMovement();
+    }
+
+    private void handleMovement()
+    {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
-            myRigidBody.velocity = new UnityEngine.Vector2(speed, velocity.y);
+            myRigidBody.velocity = new UnityEngine.Vector2(speed, myRigidBody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
-            myRigidBody.velocity = new UnityEngine.Vector2(-speed, velocity.y);
+            myRigidBody.velocity = new UnityEngine.Vector2(-speed, myRigidBody.velocity.y);
+        }
+
+        if (myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity -= friction;
+        }
+        if (myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity += friction;
+        }
+    }
+
+    private void handleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = UnityEngine.Vector2.up * jumpForce;
         }
     }
 }
